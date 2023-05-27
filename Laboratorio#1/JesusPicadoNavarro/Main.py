2bc245726d7f13272d515d0ebee17c52a8aecdc0
“[Jesus <[Jesuspicado70@gmail.com]>
2023-05-27 13:46:18 -0600

    lab1 busqueda de numeros pares con limite

--- Laboratorio#1/JesusPicadoNavarro/Main.py
+++ Laboratorio#1/JesusPicadoNavarro/Main.py
@@ -1,1 +1,13 @@
-print("Hola Mundo")
+def par (numero):
+    if numero < 2 :
+        return False
+    for i in range(1, numero):
+        if numero % 2 != 0:
+            return False
+    return True
+    
+limit=int(input("Ingrese un numero para emplearlo como limite y evaluar los numeros pares dentro del limite "))
+print("Números Pares hasta el límite", limit, ":")
+for a in range(1, limit+1):
+    if par(a):
+        print(a) 
